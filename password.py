b10794feea9877030b61ed401f21a65bba96c998
Montreal Friends <99892186+mtlfriends@users.noreply.github.com>
2022-07-26 20:09:02 -0400

    Update password.py

--- password.py
+++ password.py
@@ -1,7 +1,10 @@
 import os, hashlib
 
 def salted_password(user_password=input("Please enter your password: "), salt=os.urandom(32).hex()):
-    return hashlib.sha256(user_password.encode() + salt.encode()).hexdigest()
+    salted_user_password = user_password + salt
+    hashed_password = hashlib.sha256(salted_user_password.encode()).hexdigest()
+
+    return hashed_password
 
 salted_password()
  
